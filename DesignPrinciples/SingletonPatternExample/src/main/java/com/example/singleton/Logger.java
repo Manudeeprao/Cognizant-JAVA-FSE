6798c1cd52c44a348824ecb2f6587c0393785c19
Manudeep Rao <jupallymanudeep@gmail.com>
2026-06-12 19:19:12 +0530

    Added Singleton Pattern hands-on

---
+++ DesignPrinciples/SingletonPatternExample/src/main/java/com/example/singleton/Logger.java
@@ -0,0 +1,20 @@
+package com.example.singleton;
+
+public class Logger {
+    private static Logger instance;
+
+    private Logger() {
+        // Private constructor prevents instantiation from outside
+    }
+
+    public static synchronized Logger getInstance() {
+        if (instance == null) {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void log(String message) {
+        System.out.println("LOG: " + message);
+    }
+}
